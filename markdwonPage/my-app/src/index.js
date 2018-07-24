c15402f9c83821e9f2a450efd67b4e71edd29a4c
lidia-saf <lidiia.safiullina@yandex.ru>
2018-07-24 18:47:34 +0300

    started structuring redux

--- markdwonPage/my-app/src/index.js
+++ markdwonPage/my-app/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { createStore } from 'redux';
 import AppLayout from './AppLayout'
 
-
+const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
