05b84db1625a935ca4e5e012875ab4f7cd518094
lidia-saf <lidiia.safiullina@yandex.ru>
2018-07-30 21:10:35 +0300

    changed the reducer

--- markdwonPage/my-app/src/reducer.js
+++ markdwonPage/my-app/src/reducer.js
@@ -1,9 +1,11 @@
 import {ADD_EDITOR} from './actions/index'
 
 
-export const reducer = (state, action) => {
+export const reducer = (state = [], action) => {
     switch (action.type) {
         case ADD_EDITOR:
+          return [...state, {text: action.text}];
+        default:
           return state;
     }
 }
