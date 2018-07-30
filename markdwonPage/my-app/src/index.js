eee999f10c25f928c41401b38fd86134f1d5d68f
lidia-saf <lidiia.safiullina@yandex.ru>
2018-07-30 21:42:02 +0300

    small changes

--- markdwonPage/my-app/src/index.js
+++ markdwonPage/my-app/src/index.js
@@ -5,7 +5,11 @@ import { createStore } from 'redux';
 import AppLayout from './AppLayout'
 import { reducer } from './reducer'
 
-const store = createStore(reducer + window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const initialState = [];
+
+const store = createStore(reducer, initialState,
+  +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
