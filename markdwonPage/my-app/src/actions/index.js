b64ef5d006b65f3ef9c1334fb3b4fc0202105936
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-15 19:19:59 +0300

    edited actions list for the app

--- markdwonPage/my-app/src/actions/index.js
+++ markdwonPage/my-app/src/actions/index.js
@@ -1,18 +1,6 @@
 export const ADD_EDITOR = 'ADD_EDITOR';
-export const ADD_PREVIEW = 'ADD_PREVIEW';
-export const TRANSPILE_TEXT = 'TRANSPILE_TEXT';
 
 export const addEditor = text => ({
   type: ADD_EDITOR,
-  text
-});
-
-export const addPreview = text => ({
-  type: ADD_PREVIEW,
-  text
-});
-
-export const transpileText = text => ({
-  type: TRANSPILE_TEXT,
   text
 });
