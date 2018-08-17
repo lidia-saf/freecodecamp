af9b06913392ae38d41d9c325f09d99220f9b5f2
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-17 19:55:08 +0300

    before transferring into redux state control

---
+++ drumMachine/my-app/src/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import './index.css';
+
+const Display = () => {
+    return (
+      <div id="display">
+      Hey
+      </div>
+    )
+}
+
+export default Display;
--- drumMachine/my-app/src/MachineContainer.js
+++ drumMachine/my-app/src/MachineContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Pads from './Pads';
+import Display from './Display';
 
 const MachineContainer = () => {
   return(
     <div id="drum-machine">
       <Pads />
+      <Display />
     </div>
   )
 };
