4cea457e5f2584f921af45729432c7d5893e9af6
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-25 20:20:55 +0300

    added event listeners for equal button

--- javascript-calculator/src/Presentational/Equal.js
+++ javascript-calculator/src/Presentational/Equal.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import '../Styles/index.css';
+import { produceResult } from '../actions';
+import { connect } from 'react-redux';
 
-const Equal = () => {
+const Equal = ({calculateResult}) => {
+  function handleClick(event) {
+    calculateResult(event.target.value);
+  }
   return (
     <div>
-      <button id="equals" className="equal" type="button" value="=">=</button>
+      <button id="equals" className="equal" type="button" value="=" onClick={handleClick}>=</button>
     </div>
   )
 }
 
-export default Equal;
+const mapStateToProps = state => {
+  return {
+    input: state.input
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    calculateResult: value =>
+      dispatch(produceResult(value))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Equal);
--- javascript-calculator/src/actions.js
+++ javascript-calculator/src/actions.js
@@ -1,5 +1,6 @@
 export const ADD_INPUT = 'ADD_INPUT';
 export const CLEAR_INPUT = 'CLEAR_INPUT';
+export const PRODUCE_RESULT = 'PRODUCE_RESULT';
 
 export const addInput = value => {
   return {
@@ -12,5 +13,11 @@ export const clearInput = value => {
   return {
     type: CLEAR_INPUT,
     payload: value
+  }
+}
+
+export const produceResult = () => {
+  return {
+    type: PRODUCE_RESULT
   }
 }
