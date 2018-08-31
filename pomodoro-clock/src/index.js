8f5282810026826f4b4c468d26fcb8b0cd26da1a
lidia-saf <lidiia.safiullina@yandex.ru>
2018-08-31 21:32:52 +0300

    initial commit for pomodoro app for FCC

---
+++ pomodoro-clock/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
+import reducer from './reducers/reducer'
+import Pomodoro from './presentational/Pomodoro'
+
+import './Styles/index.css'
+
+export const initialState = {
+  input: "0",
+  output: "0"
+};
+
+const store = createStore(
+    reducer, initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   );
+
+   ReactDOM.render(
+    <Provider store={store}>
+      <Pomodoro />
+    </Provider>,
+    document.getElementById("root")
+  );
+  
